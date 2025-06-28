1d2a0d468fd7dd4cc27f46ca60fca3b8a85c74b8
mohammadhossainjoy <joyh2755@gmail.com>
2025-06-28 20:06:05 +0600

    C# Programing practice

---
+++ DataTypeExample/DataTypeExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataTypeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            double height = 5.8;
+            bool isMaried = false;
+            char grade = 'A';
+            string name = "Joy";
+
+
+            Console.WriteLine("age:" + age);
+            Console.WriteLine("height:" + height);
+            Console.WriteLine("Maried:" + isMaried);
+            Console.WriteLine("Grade:" + grade);
+            Console.WriteLine("Name:" + name);
+
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HelloWorld
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Programing World");
+        }
+    }
+}
