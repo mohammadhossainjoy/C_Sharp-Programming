c3764aaea8128cf4849fa289b8126a7a92409151
mohammadhossainjoy <joyh2755@gmail.com>
2025-07-28 00:08:30 +0600

    variable string,int,double,char,bool

---
+++ variable/variable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string university = "North South University";
+            int cseStudent = 5000;
+            int eeestudent = 3500;
+            double cgpa = 3.20;
+            bool isStudent = true;
+            char letter= 'A';
+
+            int eceTotalStudent = cseStudent + eeestudent;
+            Console.WriteLine(university);
+            Console.WriteLine("Total Student: "+ eceTotalStudent);
+            Console.WriteLine("Cgpa: "+cgpa);
+            Console.WriteLine("isStudent: " + isStudent);
+            Console.WriteLine("Letter: "+letter);
+      
+            Console.ReadKey();
+        }
+    }
+}
+
