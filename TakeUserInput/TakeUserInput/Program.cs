37bb27a79104a715586914c63dcd8c0376a89e46
mohammadhossainjoy <joyh2755@gmail.com>
2025-07-28 00:26:53 +0600

    Taking User Input

---
+++ TakeUserInput/TakeUserInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TakeUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please Enter first number: ");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please Enter Second tnumber: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result=num1 + num2; 
+            
+            Console.WriteLine("Result: "+result);
+        }
+    }
+}
