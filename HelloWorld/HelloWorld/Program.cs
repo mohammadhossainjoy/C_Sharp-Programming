9a2acc6e5c29d4d4ea048720da6f1649bef75545
mohammadhossainjoy <joyh2755@gmail.com>
2025-07-27 23:43:37 +0600

    Hello World programing

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,3 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using System;
+
+namespace HelloWorld;
+
+class Program
+{
+        static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+}
