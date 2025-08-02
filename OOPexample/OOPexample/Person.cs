ab674e4370a180d4fed030e3df9a0bb1308802b4
mohammadhossainjoy <joyh2755@gmail.com>
2025-08-02 23:21:26 +0600

    class and object,properties,method example

--- OOPexample/OOPexample/Person.cs
+++ OOPexample/OOPexample/Person.cs
@@ -4,18 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOPexample
+namespace ClassandObject
 {
     internal class Person
     {
-        public string firstName;
+        private string firstName;
         public string middlename;
         public string lastname;
+        private int age;
+
+        //Properties use
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if(value >= 18)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Child");
+                }
+            }
+        }
 
         public string GetFullName()
         {
             string FullName = firstName + " " + middlename + " " + lastname;
             return FullName;
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("Age is :" + age);
         }
     }
 }
--- OOPexample/OOPexample/Program.cs
+++ OOPexample/OOPexample/Program.cs
@@ -5,12 +5,14 @@
         static void Main(string[] args)
         {
             Person person1 = new Person();
-            person1.firstName = "Mohammad";
+            person1.FirstName = "Mohammad";
             person1.middlename = "Hossain";
             person1.lastname = "Joy";
-
+            person1.Age = 23;
+          
             string fullname=person1.GetFullName();
             Console.WriteLine("FullName is : "+fullname);
+            person1.showData();
             Console.ReadKey();
         }
     }
