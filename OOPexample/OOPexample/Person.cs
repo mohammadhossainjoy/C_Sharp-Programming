0b4f11f3a722abf59df4fb72f90b7f880dde618e
mohammadhossainjoy <joyh2755@gmail.com>
2025-08-02 22:00:37 +0600

    Class and object example

---
+++ OOPexample/OOPexample/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPexample
+{
+    internal class Person
+    {
+        public string firstName;
+        public string middlename;
+        public string lastname;
+
+        public string GetFullName()
+        {
+            string FullName = firstName + " " + middlename + " " + lastname;
+            return FullName;
+        }
+    }
+}
---
+++ OOPexample/OOPexample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OOPexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.firstName = "Mohammad";
+            person1.middlename = "Hossain";
+            person1.lastname = "Joy";
+
+            string fullname=person1.GetFullName();
+            Console.WriteLine("FullName is : "+fullname);
+            Console.ReadKey();
+        }
+    }
+}
